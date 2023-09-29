689f415b610c11fa4eccce4ff162cc9a5b36133d
marvin <marvin_zuniga19@outlook.com>
2023-09-29 10:14:32 -0600

    mas notas

---
+++ 04_list_iterables_sets.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  // List (Listas)
+
+  final numeros: [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 9, 10 ];
+  print('Lista original: $numeros'); // imprime lista original de numeros
+
+  print('Length ${numeros.length}'); // cantidad de caracteres en la lista
+
+  print('indice 0 es: ${numeros[0]}'); // corchete selecciona valor dentro de la lista
+
+  print('first: ${numeros.first}'); // obtiene primer valor
+
+  print('last: ${numeros.last}') // obtiene ultimo valor lista
+
+  print('reversed: ${numeros.reversed}') // Ordena de manera inversa 
+}
